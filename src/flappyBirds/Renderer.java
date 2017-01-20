40152478b1abc95945849a2d3c1a2e257a7e105d
Yash Bora <yash.bora@hotmail.com>
2017-01-19 19:56:39 -0600

    Start

---
+++ src/flappyBirds/Renderer.java
@@ -0,0 +1,16 @@
+package flappyBirds;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		
+		FlappyBirds.flappyBird.repaint(g);
+	}
+}
